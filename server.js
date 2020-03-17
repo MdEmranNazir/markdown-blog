050fd8c45e1a44c973c954b0be3d86891fe9596b
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-03-17 08:18:54 +0600

    model work end

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const ArticleRouter = require('./routes/articles');
-
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
 const app = express();
 
 //DB Connect
 mongoose.connect('mongodb://localhost/markdown-blog', {
-    useNewUrlParser: true, useUnifiedTopology: true
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 });
 
 //view engine
-app.set('view engine');
+app.set('view engine', 'ejs');
 
 //Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use('/articles', ArticleRouter);
+app.use(methodOverride('_method'));
 
-// Router
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test description 2'
-        }];
-    res.render('articles/index.ejs',{ articles: articles })
-    
+// Home route
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', { articles: articles })
 });
+
+app.use('/articles', articleRouter);
 
 // PORT
 const PORT = process.env.PORT || 5000;
